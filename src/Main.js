a844724d92c7ca549c998f39ecbc882def216b9a
Ilona <white255orchids@gmail.com>
2022-08-16 14:37:20 -0400

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 <div>
             <h1>“Overpower. Overtake. Overcome.”</h1>
-            <img className = "frontImage" src = "Bike3.webp" alt = "bike"/>
-            <img className = "frontImage" src = "RidingBike.webp" alt= "bike"/>
-            <img className = "frontImage" src = "Bike4.webp" alt = "bike"/>
+            <img className = "frontImage" src = "bike3.webp" alt = "bike"/>
+            <img className = "frontImage" src = "riding bike.webp" alt= "bike"/>
+            <img className = "frontImage" src = "bike4.webp" alt = "bike"/>
         </div>
