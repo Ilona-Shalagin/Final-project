6418f1951de7407cb17c47077e5a162570db9f99
Ilona <white255orchids@gmail.com>
2022-08-16 15:08:47 -0400

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,11 @@
-<div>
+const Main = () => {
+    return(
+        <div>
             <h1>“Overpower. Overtake. Overcome.”</h1>
             <img className = "frontImage" src = "bike3.webp" alt = "bike"/>
             <img className = "frontImage" src = "riding bike.webp" alt= "bike"/>
             <img className = "frontImage" src = "bike4.webp" alt = "bike"/>
         </div>
+    )
+}
+export default Main;
