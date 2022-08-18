52ab04e6c6b6d36850a39d2c9c2bcc39fd36e1d7
Ilona <white255orchids@gmail.com>
2022-08-17 20:08:30 -0400

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 const Main = () => {
     return(
         <div>
-            <h1className="quote">“Overpower. Overtake. Overcome.”</h1>
+            <h1 className="quote">“Overpower. Overtake. Overcome.”</h1>
             <img className = "frontImage" src = "bike3.webp" alt = "bike"/>
             <img className = "frontImage" src = "riding bike.webp" alt= "bike"/>
             <img className = "frontImage" src = "bike4.webp" alt = "bike"/>
